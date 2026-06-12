08bbd6fc0b1ec1564fcfac9d9ffdd1d5d03bc505
danny9883 <danny9883@naver.com>
2026-06-12 12:54:44 +0900

    Repository 생성 / 방명록 작성 기능 추가

--- src/main/java/com/example/springedu2/controller/VisitorController.java
+++ src/main/java/com/example/springedu2/controller/VisitorController.java
@@ -1,9 +1,52 @@
 package com.example.springedu2.controller;
 
+import com.example.springedu2.entity.Visitor;
+import com.example.springedu2.repository.VisitorRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class VisitorController {
-    
+
+    @Autowired
+    private VisitorRepository visitorRepository;
+
+    @GetMapping("/vlist")
+    public ModelAndView vlist() {
+        return null;
+    }
+
+    @GetMapping("/vsearch")
+    public ModelAndView vsearch() {
+        return null;
+    }
+
+    // @Valid : form 에서 넘어온 자료를 @Entity 에 있는
+    // 설정( @Id, @NotBlank, @Column(nullable=false) )
+    // 과 비교해서, 입력 data를 검증한다
+    @PostMapping("/vinsert")
+    @Transactional
+    public String vinsert(@Valid Visitor visitor,
+                          BindingResult bindingResult,
+                          Model model) {
+
+        System.out.println("visitor" + visitor);
+        System.out.println("bindingResult" + bindingResult);
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("msg", "이름과 내용을 모두 입력하세요");
+            return "visitorView"; // visitorView.html
+        }
+        visitorRepository.save(visitor); // entity 객체를 사용해야한다.
+
+        return "redirect:/vlist";
+    }
+
 }
 
